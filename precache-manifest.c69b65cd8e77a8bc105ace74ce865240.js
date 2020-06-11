843ff4739defc3057af1f30a20f03eff31a12717
colorswall <info@colorswall.com>
2020-06-11 12:54:47 +0300

    update

---
+++ precache-manifest.c69b65cd8e77a8bc105ace74ce865240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b0043c90d97ec036d090f7b4a1769b",
+    "url": "/colorpick/index.html"
+  },
+  {
+    "revision": "4f5ad59b6417708eae7d",
+    "url": "/colorpick/static/css/main.a6f79ef7.chunk.css"
+  },
+  {
+    "revision": "abb51fd853e177bbfee4",
+    "url": "/colorpick/static/js/2.9bf085c9.chunk.js"
+  },
+  {
+    "revision": "38a4b558739197dd9ec900c0561b7e3e",
+    "url": "/colorpick/static/js/2.9bf085c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f5ad59b6417708eae7d",
+    "url": "/colorpick/static/js/main.b29cd512.chunk.js"
+  },
+  {
+    "revision": "09447548f479378a83b2",
+    "url": "/colorpick/static/js/runtime-main.342f1d4e.js"
+  }
+]);
